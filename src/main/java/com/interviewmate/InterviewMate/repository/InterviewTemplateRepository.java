5e3490b248994f50b3733afd1d5d70dee32cb7eb
camilo661 <camilocastropsn@gmail.com>
2026-03-26 10:32:47 -0500

    Add InterviewSessionRepository and InterviewTemplateRepository interfaces for CRUD operations

---
+++ src/main/java/com/interviewmate/InterviewMate/repository/InterviewTemplateRepository.java
@@ -0,0 +1,9 @@
+package com.interviewmate.InterviewMate.repository;
+
+import com.interviewmate.InterviewMate.entity.InterviewTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface InterviewTemplateRepository extends JpaRepository<InterviewTemplate, UUID> {
+}
