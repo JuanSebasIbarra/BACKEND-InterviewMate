8b312b9b83b2eca3c11c9203087f077bb4e80fb6
camilo661 <camilocastropsn@gmail.com>
2026-03-26 11:46:19 -0500

    Add method to find InterviewTemplates by user ID

--- src/main/java/com/interviewmate/InterviewMate/repository/InterviewTemplateRepository.java
+++ src/main/java/com/interviewmate/InterviewMate/repository/InterviewTemplateRepository.java
@@ -3,7 +3,9 @@ package com.interviewmate.InterviewMate.repository;
 import com.interviewmate.InterviewMate.entity.InterviewTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InterviewTemplateRepository extends JpaRepository<InterviewTemplate, UUID> {
+    List<InterviewTemplate> findByUserId(Long userId);
 }
