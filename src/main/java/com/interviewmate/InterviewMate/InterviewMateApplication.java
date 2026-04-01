59c533afc39f46d0b80b9edc031107d3c9c9ca9f
JuanSebasIbarra <juan.ibarrasalas@campusucc.edu.co>
2026-04-01 15:02:13 -0500

    Add ConfigurationPropertiesScan annotation to InterviewMateApplication

--- src/main/java/com/interviewmate/InterviewMate/InterviewMateApplication.java
+++ src/main/java/com/interviewmate/InterviewMate/InterviewMateApplication.java
@@ -2,8 +2,10 @@ package com.interviewmate.InterviewMate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class InterviewMateApplication {
 
 	public static void main(String[] args) {
