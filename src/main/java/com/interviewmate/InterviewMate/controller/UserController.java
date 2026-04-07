4169165269ba9fe656531ed057c72331d36f74de
JuanSebasIbarra <juan.ibarrasalas@campusucc.edu.co>
2026-04-06 19:49:42 -0500

    Add API versioning to UserController endpoint mapping

--- src/main/java/com/interviewmate/InterviewMate/controller/UserController.java
+++ src/main/java/com/interviewmate/InterviewMate/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping({"/usuarios", "/api/v1/users"})
 @RequiredArgsConstructor
 public class UserController {
 
