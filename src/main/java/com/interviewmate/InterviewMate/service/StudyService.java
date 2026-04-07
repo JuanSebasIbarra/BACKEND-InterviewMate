ef81106a675d266e981df2d1c9867c2e183f0650
JuanSebasIbarra <juan.ibarrasalas@campusucc.edu.co>
2026-04-06 19:50:49 -0500

    Add getByAuthenticatedUser and transcribeAudio methods to StudyService interface

--- src/main/java/com/interviewmate/InterviewMate/service/StudyService.java
+++ src/main/java/com/interviewmate/InterviewMate/service/StudyService.java
@@ -3,12 +3,19 @@ package com.interviewmate.InterviewMate.service;
 import com.interviewmate.InterviewMate.dto.GenerateStudyQuestionsRequest;
 import com.interviewmate.InterviewMate.dto.StartStudyRequest;
 import com.interviewmate.InterviewMate.dto.StudySessionResponse;
+import com.interviewmate.InterviewMate.dto.StudySessionSummaryResponse;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StudyService {
     StudySessionResponse start(StartStudyRequest request);
     StudySessionResponse generateQuestions(GenerateStudyQuestionsRequest request);
     StudySessionResponse getById(UUID studySessionId);
+    List<StudySessionSummaryResponse> getByAuthenticatedUser();
+    String transcribeAudio(MultipartFile audioFile);
 }
+
+
 
